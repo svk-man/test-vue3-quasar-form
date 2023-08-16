b392182d34459d0dc95ece7549cf8d11216e49f1
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-08-16 14:05:25 +0300

    feat: add qdialog

---
+++ src/components/UserForm.vue
@@ -0,0 +1,101 @@
+<template>
+  <div
+    class="q-pa-md full-width bg-white shadow-1 rounded-borders"
+    style="max-width: 500px"
+  >
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="name"
+        label="Ваше имя *"
+        hint="Имя"
+        lazy-rules
+        :rules="[
+          (val) => !!val || 'Пожалуйста, введите имя',
+          (val) =>
+            val.length <= 100 || 'Имя должно содержать не более 100 символов',
+        ]"
+      ></q-input>
+
+      <q-input
+        filled
+        v-model="surname"
+        label="Ваша фамилия"
+        hint="Фамилия"
+        lazy-rules
+        :rules="[
+          (val) =>
+            val.length <= 100 ||
+            'Фамилия должна содержать не более 100 символов',
+        ]"
+      ></q-input>
+
+      <q-input
+        filled
+        v-model="phone"
+        label="Телефон *"
+        mask="+7(###) ### - ## - ##"
+        hint="Формат: +7(###) ### - ## - ##"
+        lazy-rules
+        :rules="[
+          (val) =>
+            (val && val.length === '+7(###) ### - ## - ##'.length) ||
+            'Пожалуйста, введите телефон',
+        ]"
+      ></q-input>
+
+      <q-input
+        filled
+        v-model="email"
+        label="Email *"
+        type="email"
+        hint="Email"
+        lazy-rules
+        :rules="[(val) => !!val || 'Пожалуйста, введите email', isValidEmail]"
+      ></q-input>
+
+      <q-input
+        v-model="message"
+        filled
+        type="textarea"
+        lazy-rules
+        :rules="[
+          (val) =>
+            val.length <= 500 ||
+            'Сообщение должно содержать не более 500 символов',
+        ]"
+      ></q-input>
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"></q-btn>
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        ></q-btn>
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent, ref } from "vue";
+
+defineComponent({
+  name: "UserForm",
+});
+
+const name = ref("");
+const surname = ref("");
+const phone = ref("");
+const email = ref("");
+const message = ref("");
+
+function isValidEmail(val) {
+  const emailPattern =
+    /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+  return emailPattern.test(val) || "Некорректный email";
+}
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,100 +1,23 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="q-pa-md full-width" style="max-width: 500px">
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input
-          filled
-          v-model="name"
-          label="Ваше имя *"
-          hint="Имя"
-          lazy-rules
-          :rules="[
-            (val) => !!val || 'Пожалуйста, введите имя',
-            (val) =>
-              val.length <= 100 || 'Имя должно содержать не более 100 символов',
-          ]"
-        ></q-input>
-
-        <q-input
-          filled
-          v-model="surname"
-          label="Ваша фамилия"
-          hint="Фамилия"
-          lazy-rules
-          :rules="[
-            (val) =>
-              val.length <= 100 ||
-              'Фамилия должна содержать не более 100 символов',
-          ]"
-        ></q-input>
-
-        <q-input
-          filled
-          v-model="phone"
-          label="Телефон *"
-          mask="+7(###) ### - ## - ##"
-          hint="Формат: +7(###) ### - ## - ##"
-          lazy-rules
-          :rules="[
-            (val) =>
-              (val && val.length === '+7(###) ### - ## - ##'.length) ||
-              'Пожалуйста, введите телефон',
-          ]"
-        ></q-input>
-
-        <q-input
-          filled
-          v-model="email"
-          label="Email *"
-          type="email"
-          hint="Email"
-          lazy-rules
-          :rules="[(val) => !!val || 'Пожалуйста, введите email', isValidEmail]"
-        ></q-input>
-
-        <q-input
-          v-model="message"
-          filled
-          type="textarea"
-          lazy-rules
-          :rules="[
-            (val) =>
-              val.length <= 500 ||
-              'Сообщение должно содержать не более 500 символов',
-          ]"
-        ></q-input>
-
-        <div>
-          <q-btn label="Submit" type="submit" color="primary"></q-btn>
-          <q-btn
-            label="Reset"
-            type="reset"
-            color="primary"
-            flat
-            class="q-ml-sm"
-          ></q-btn>
-        </div>
-      </q-form>
-    </div>
+    <q-btn
+      label="Открыть модальное окно"
+      color="primary"
+      @click="form = true"
+    ></q-btn>
+    <q-dialog v-model="form">
+      <user-form />
+    </q-dialog>
   </q-page>
 </template>
 
 <script setup>
 import { defineComponent, ref } from "vue";
+import UserForm from "../components/UserForm.vue";
 
 defineComponent({
   name: "IndexPage",
 });
 
-const name = ref("");
-const surname = ref("");
-const phone = ref("");
-const email = ref("");
-const message = ref("");
-
-function isValidEmail(val) {
-  const emailPattern =
-    /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
-  return emailPattern.test(val) || "Некорректный email";
-}
+const form = ref(false);
 </script>
