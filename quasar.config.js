ffcce8aeaf9f54e849b107efe5c08df23b8fe68c
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-08-16 14:50:57 +0300

    fix: fix error with publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: "/test-vue3-quasar-form/",
       // analyze: true,
       // env: {},
       // rawDefine: {}
